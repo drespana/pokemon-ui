b569405128e911e625cc8001e1a62ca5ad4d46e4
drespana <drespana@google.com>
2024-03-22 11:41:29 -0500

    pokemon card ui project

---
+++ poke-ui/src/app/poke-home/poke-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-home',
+  templateUrl: './poke-home.component.html',
+  styleUrl: './poke-home.component.css'
+})
+export class PokeHomeComponent {
+
+}
