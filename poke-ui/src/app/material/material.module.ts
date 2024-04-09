0779f87c6d10089d75c1c4defa421fa99568031a
drespana <drespana@google.com>
2024-04-09 17:13:27 -0500

    poke-navigation component opens a side-nav to navigate to unpopulated components

--- poke-ui/src/app/material/material.module.ts
+++ poke-ui/src/app/material/material.module.ts
@@ -3,14 +3,16 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button"
 import { MatIconModule } from "@angular/material/icon"
-
+import { MatSidenavModule } from '@angular/material/sidenav'
 
 
 const MaterialComponents = [
     MatCardModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    
 ]
 
 @NgModule({
--- poke-ui/src/app/poke-navigation/poke-navigation.component.ts
+++ poke-ui/src/app/poke-navigation/poke-navigation.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'poke-navigation',
   templateUrl: './poke-navigation.component.html',
-  styleUrl: './poke-navigation.component.css'
+  styleUrl: './poke-navigation.component.css',
 })
 export class PokeNavigationComponent {
-
+  opened: boolean = false;
 }
